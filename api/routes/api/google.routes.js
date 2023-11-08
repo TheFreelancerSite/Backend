8bf191f88ff4ec6a0c1e2dacdb8ff131721c55f6
Abderrahmen <abderrahmenlouhichi3@gmail.com>
2023-11-08 18:29:03 +0100

    add google Auth with passport

---
+++ api/routes/api/google.routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = require('express').Router();
+const passport = require("passport")
+
+
+router.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile',"emails"] }));
+
+router.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: 'http://localhost:5173/signin' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('http://localhost:5173');
+  });
+
+module.exports = router;
--- api/routes/api/user.routes.js
+++ api/routes/api/user.routes.js
@@ -1,10 +1,24 @@
 const router = require('express').Router();
-const { signin, createProfile, getUser } = require('../../controllers/users');
+const passport = require("passport")
+
+const { signin, createProfile, getUser,updateUserProfile} = require('../../controllers/users');
 const multer = require('multer');
 const upload = multer();
 
 router.get('/getUser/:userId',getUser)
 router.post('/signin', signin);
 router.post('/signup', upload.single('image'), createProfile); 
+router.put('/updateUser/:userId', upload.single('image'),updateUserProfile)
+
+
+router.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+router.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: 'http://localhost:5173/signin' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('http://localhost:5173');
+  });
 
 module.exports = router;
