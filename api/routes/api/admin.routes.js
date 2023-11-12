a8394d6eda68875723e51a83ae551d79ebffe328
Haithemch98 <chaouchhaithem0@gmail.com>
2023-11-13 00:08:33 +0100

    setting the admin rooutes

--- api/routes/api/admin.routes.js
+++ api/routes/api/admin.routes.js
@@ -1,10 +1,14 @@
 // admin.routes.js
 const router = require('express').Router();
-const { authenticateAdmin } = require('../../controllers/admin');
-const multer = require('multer');
-const { adminAuthenticated } = require('../../middlewares/Auth.middlewares');
+const { authenticateAdmin, getUserById, getclients, getfreelancer } = require('../../controllers/admin');
 
 
-router.post('/login',authenticateAdmin);
+
+router.post('/login', authenticateAdmin);
+router.get('/user', getUserById)
+router.get('/clients', getclients)
+router.get('/freelancers', getfreelancer)
+
+
 
 module.exports = router;
