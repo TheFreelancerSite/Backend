b928550b99a939b583a4557b1cbe28c4fbd792be
Abderrahmen <abderrahmenlouhichi3@gmail.com>
2023-11-12 16:42:01 +0100

    fix google auth controller and routes

--- api/routes/api/google.routes.js
+++ api/routes/api/google.routes.js
@@ -1,16 +1,35 @@
 const express = require("express");
-const router = require('express').Router();
-const passport = require("passport")
+const router = express.Router();
+const passport = require("passport");
 
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-router.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile',"email"] }));
-
-router.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: 'http://localhost:5173/signin' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('http://localhost:5173');
-  });
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "http://localhost:5173/login",
+  }),
+  function (req, res) {
+    if (req.user) {
+      const user = {  
+        userId: req.user.id,
+        userName: req.user.userName,
+        email: req.user.email,
+        imgUrl: req.user.imgUrl,
+        isSeller : req.user.isSeller
+      };
+      if(user.isSeller === false){
+      res.redirect(`http://localhost:5173/freelancerHomePage?user=${user.userId}&isSeller=${user.isSeller}&imgUrl=${user.imgUrl}&userName=${user.userName}`)
+      }else if(user.isSeller === true){
+        res.redirect(`http://localhost:5173/clientHomePage?user=${user.userId}&isSeller=${user.isSeller}&imgUrl=${user.imgUrl}&userName=${user.userName}`)
+      }
+    } else {
+      res.redirect("http://localhost:5173/login");
+    }
+  }
+);
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -10,14 +10,22 @@ const upload = multer({ dest: "uploads/" });
 require("dotenv").config();
 
 const app = express();
-
-app.use(cors());
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://localhost:3000'], 
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
+
+const sessionSecret = process.env.SESSION_SECRET || "secret";
+
 app.use(
   session({
-    secret: "sercret",
+    secret: sessionSecret,
     resave: false,
     saveUninitialized: false,
   })
