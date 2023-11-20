65c4b8f0b45f5b65829f84ed4b788c7e333638a3
hechsb <hichem2001.sboui@gmail.com>
2023-11-20 20:26:21 +0100

    merging

--- api/controllers/payment.js
+++ api/controllers/payment.js
@@ -11,7 +11,7 @@ module.exports = {
 
         try {
             const paymentIntent = await stripe.paymentIntents.create({
-                amount: amount,
+                amount: amount*100,
                 currency: 'usd',    
                 automatic_payment_methods: {
                     enabled: true
